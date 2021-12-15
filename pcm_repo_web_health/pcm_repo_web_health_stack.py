2be576d52dde1dbbfaae9e30afbcb4bc2c1fbe12
Ubuntu <ubuntu@ip-172-31-40-192.us-east-2.compute.internal>
2021-12-15 15:47:20 +0000

    cloudwatch for periodic lambda execution and moitoring

--- pcm_repo_web_health/pcm_repo_web_health_stack.py
+++ pcm_repo_web_health/pcm_repo_web_health_stack.py
@@ -2,8 +2,9 @@ from aws_cdk import (
     core as cdk,
     # aws_ec2 as ec2,
     aws_lambda as _lambda,
-    aws_events as aws_events,
-    aws_events_targets as aws_events_targets
+    aws_events as _events,
+    aws_events_targets as _events_targets,
+    aws_iam as aws_iam
 )
 
 # For consistency with other languages, `cdk` is the preferred import name for
@@ -20,17 +21,34 @@ class PcmRepoWebHealthStack(cdk.Stack):
         
         # Creating a Lambda function, which calls the Hello World Handler
         # HW_lambda = self.create_lambda("Hello World!", "./resources/", "HelloWorld_lambda.lambda_handler")
-        
+        lambda_role = self.create_lambda_role()
         # Creating a Lambda function, which creates and calls the WebHealth Lambda Function
-        WH_lambda = self.create_lambda("Hello World!", "./resources/", "webHealth_lambda.lambda_handler")
-         
-    def create_lambda(self, id, asset, handler):
+        WH_lambda = self.create_lambda("Hello World!", "./resources/", "webHealth_lambda.lambda_handler", lambda_role)
+        
+        lambda_schedule = _events.Schedule.rate(cdk.Duration.minutes(1))
+        lambda_targets = _events_targets.LambdaFunction(handler=WH_lambda)
+        rule = _events.Rule(self, "webHealth_Invocation", description="Periodic Lambda", enabled=True, schedule=lambda_schedule, targets=[lambda_targets])
+    
+    
+    def create_lambda_role(self):
+        lambdaRole = aws_iam.Role(self, "lambda-role",
+                        assumed_by = aws_iam.ServicePrincipal('lambda.amazonaws.com'),
+                        managed_policies=[
+                            aws_iam.ManagedPolicy.from_aws_managed_policy_name('service-role/AWSLambdaBasicExecutionRole'),
+                            aws_iam.ManagedPolicy.from_aws_managed_policy_name('CloudWatchFullAccess'),
+                        ])
+            
+        return lambdaRole
+        
+        
+    def create_lambda(self, id, asset, handler, role):
         ### Creates a lambda function in python3.6
         return _lambda.Function(self, 
         id,
         handler=handler,  # optional, defaults to 'handler'
         runtime=_lambda.Runtime.PYTHON_3_6,
-        code=_lambda.Code.from_asset(asset)
+        code=_lambda.Code.from_asset(asset),
+        role=role
         )
     
    
--- resources/webHealth_lambda.py
+++ resources/webHealth_lambda.py
@@ -1,13 +1,25 @@
 import datetime
 import urllib3
-
-#global since everyone accesses it
-URL_TO_MONITOR = "www.skipq.org"
-    
+import constants as constants
+from cloudWatch_putMetric import CloudWatchPutMetric
+     
 def lambda_handler(event, context):
     values = dict()
+    
+    cw = CloudWatchPutMetric();
+    
+    dimensions = [
+        {'Name': 'URL', 'Value':constants.URL_TO_MONITOR},
+        {'Name': 'Region', 'Value': "DUB" }
+        ]
+        
+        
     avail = get_availability()
+    cw.put_data(constants.URL_MONITOR_NAMESPACE, constants.URL_MONITOR_NAME_AVAILABILITY, dimensions, avail )
+    
     latency = get_latency()
+    cw.put_data(constants.URL_MONITOR_NAMESPACE, constants.URL_MONITOR_NAME_LATENCY, dimensions, latency)
+    
     values.update({
         "availability": avail,
         "latency": latency
@@ -18,7 +30,7 @@ def lambda_handler(event, context):
 def get_availability():
     ### Returns 1.0 if available and 0 if not
     http = urllib3.PoolManager()
-    response = http.request("GET", URL_TO_MONITOR)
+    response = http.request("GET", constants.URL_TO_MONITOR)
     if response.status == 200:
         return 1.0
     else:
@@ -29,7 +41,7 @@ def get_latency():
     ### Returns latency in seconds
     http = urllib3.PoolManager()
     start = datetime.datetime.now()
-    response = http.request("GET", URL_TO_MONITOR)
+    response = http.request("GET", constants.URL_TO_MONITOR)
     end = datetime.datetime.now()
     delta = end - start
     latencySec = round(delta.microseconds * .000001, 6)
