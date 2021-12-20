56e0871132aac134a09131753f0916438e4bdc3f
Ubuntu <ubuntu@ip-172-31-36-86.us-east-2.compute.internal>
2021-12-20 12:21:50 +0000

    structure changes

--- resources/HelloWorld_lambda.py
+++
@@ -1,3 +0,0 @@
-def lambda_handler(event, context):
-    # This function simply takes the First and last name as input and returns Hello f_name l_name
-    return f"Hello {event['first_name']} {event['first_name']}"
--- resources/constants.py
+++
@@ -1,7 +0,0 @@
-URL_TO_MONITOR = "www.skipq.org"
-
-URL_MONITOR_NAMESPACE = "Nimra_WebHeathMonitorPeriodic"
-URL_MONITOR_NAME_AVAILABILITY = "url_availability"
-URL_MONITOR_NAME_LATENCY = "url_latency"
-
-
